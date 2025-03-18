5557b56080c83a7f9d04db3ed9c3e28d7eb06e68
TKKmiyajima <135396758+cydiawaltz@users.noreply.github.com>
2025-03-18 21:30:10 +0900

    線形完成

---
+++ モデリング/tutuji/rename.bat
@@ -0,0 +1,3 @@
+cd C:\Users\wattz\Documents\BVE\“ú”ä’JüV\ƒ‚ƒfƒŠƒ“ƒO\tutuji\generated
+C:\Users\wattz\Documents\VSProjects\RenameAllFiles\RenameAllFiles\bin\Debug\net8.0\RenameAllFiles.exe
+_converted
